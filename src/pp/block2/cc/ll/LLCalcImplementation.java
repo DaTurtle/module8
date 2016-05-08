7aefd001b2ac576ace71716b68e2130dad94c349
F0XSQUAD <becausemysmartphone@gmail.com>
2016-05-08 17:45:31 +0200

    Ah er is zeik!

---
+++ src/pp/block2/cc/ll/LLCalcImplementation.java
@@ -0,0 +1,45 @@
+package pp.block2.cc.ll;
+
+import pp.block2.cc.NonTerm;
+import pp.block2.cc.Symbol;
+import pp.block2.cc.Term;
+
+import java.util.*;
+
+/**
+ * Created by Bas on 08/05/2016.
+ */
+public class LLCalcImplementation implements LLCalc{
+    private Grammar grammar;
+
+    public LLCalcImplementation(Grammar givenG) {
+
+        this.grammar = givenG;
+    }
+
+    @Override
+    public Map<Symbol, Set<Term>> getFirst() {
+        HashMap result = new HashMap<Symbol, Set<Term>>();
+        Set nonterm = grammar.getNonterminals();
+        List<Rule> rules  = grammar.getRules();
+        for (Rule rule: rules ) {
+            result.put(rule.getLHS(), rule.getRHS());
+        }
+        return result;
+    }
+
+    @Override
+    public Map<NonTerm, Set<Term>> getFollow() {
+        return null;
+    }
+
+    @Override
+    public Map<Rule, Set<Term>> getFirstp() {
+        return null;
+    }
+
+    @Override
+    public boolean isLL1() {
+        return false;
+    }
+}
---
+++ src/pp/block2/cp/lockcoupling/LinkedList.java
@@ -0,0 +1,7 @@
+package pp.block2.cp.lockcoupling;
+
+/**
+ * Created by Bas on 02/05/2016.
+ */
+public class LinkedList {
+}
