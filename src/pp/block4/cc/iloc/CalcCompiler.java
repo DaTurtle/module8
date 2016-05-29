777860f41c004a75de334a0a0edbfdd63ce7cfc6
Jan-Willem <janwillem.nh@gmail.com>
2016-05-29 18:42:30 +0200

    Block4 should be done

--- src/pp/block4/cc/iloc/CalcCompiler.java
+++ src/pp/block4/cc/iloc/CalcCompiler.java
@@ -7,18 +7,18 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import pp.block4.cc.ErrorListener;
-import pp.block4.cc.iloc.CalcParser;
 import pp.iloc.Simulator;
-import pp.iloc.model.Op;
-import pp.iloc.model.OpCode;
-import pp.iloc.model.Operand;
-import pp.iloc.model.Program;
+import pp.iloc.model.*;
 
 /** Compiler from Calc.g4 to ILOC. */
 public class CalcCompiler extends CalcBaseListener {
 	/** Program under construction. */
 	private Program prog;
+	private int registerCount;
+	private ParseTreeProperty<Operand> optree;
 	// Attribute maps and other fields
 
 	/** Compiles a given expression string into an ILOC program. */
@@ -47,8 +47,12 @@ public class CalcCompiler extends CalcBaseListener {
 
 	/** Compiles a given Calc-parse tree into an ILOC program. */
 	public Program compile(ParseTree tree) {
-		// TODO Fill in
-		throw new UnsupportedOperationException("Fill in");
+		prog = new Program();
+		registerCount = 0;
+		optree = new ParseTreeProperty<>();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		walker.walk(this, tree);
+		return prog;
 	}
 
 	/** Constructs an operation from the parameters 
@@ -59,16 +63,54 @@ public class CalcCompiler extends CalcBaseListener {
 
 	/** Calls the compiler, and simulates and prints the compiled program. */
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.err.println("Usage: [expr]+");
-			return;
-		}
+		String[] progs = {
+				"1+-3*4",
+				"2*3+-4"
+		};
 		CalcCompiler compiler = new CalcCompiler();
-		for (String expr : args) {
+		for (String expr : progs) {
 			System.out.println("Processing " + expr);
 			Program prog = compiler.compile(expr);
 			new Simulator(prog).run();
 			System.out.println(prog.prettyPrint());
 		}
 	}
+
+	@Override public void exitComplete(CalcParser.CompleteContext ctx) {
+		emit(OpCode.out, new Str("Outcome: "), optree.get(ctx.expr()));
+	}
+
+	@Override public void exitPar(CalcParser.ParContext ctx) {
+		optree.put(ctx, optree.get(ctx.expr()));
+	}
+
+	@Override public void exitMinus(CalcParser.MinusContext ctx) {
+		Reg r = new Reg("r_"+ registerCount);
+		optree.put(ctx, r);
+		emit(OpCode.rsubI, optree.get(ctx.expr()), new Num(0), r);
+		registerCount++;
+	}
+
+	@Override public void exitNumber(CalcParser.NumberContext ctx) {
+		Reg r = new Reg("r_"+ registerCount);
+		optree.put(ctx, r);
+		emit(OpCode.loadI, new Num(Integer.parseInt(ctx.NUMBER().getText())), r);
+		registerCount++;
+	}
+
+	@Override public void exitTimes(CalcParser.TimesContext ctx) {
+		Reg r = new Reg("r_"+ registerCount);
+		optree.put(ctx, r);
+		emit(OpCode.mult, optree.get(ctx.expr(0)), optree.get(ctx.expr(1)), r);
+		registerCount++;
+	}
+
+	@Override public void exitPlus(CalcParser.PlusContext ctx) {
+		Reg r = new Reg("r_"+ registerCount);
+		optree.put(ctx, r);
+		emit(OpCode.add, optree.get(ctx.expr(0)), optree.get(ctx.expr(1)), r);
+		registerCount++;
+	}
+
+
 }
